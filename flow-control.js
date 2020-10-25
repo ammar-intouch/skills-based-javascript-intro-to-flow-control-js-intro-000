f09d94217a7f55e22dec7dad8086bb61ea63b852
Ammar Sulaiman <ammar.sulaiman@gmail.com>
2020-10-25 11:07:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,8 +6,8 @@ function teenager(age) {if(age >=13 && age <=19) {return "You are a teenager!"}
 else {return "You are not a teenager"}
 }
 
-function ageChecker(age) {
-
+function ageChecker(age) {if(age >= 13 && age<=19) {retun "You are a teenager!"}
+else if(age <=12) {return "You are a kid"} else(age>=20) {return "You are grownup"}
 }
 
 function ternaryTeenager(age) {
